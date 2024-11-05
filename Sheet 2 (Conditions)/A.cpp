fa7607b0b054e3f603b2b05a10f367d414160c26
cewatlac <128319370+cewatlac@users.noreply.github.com>
2024-11-05 20:54:58 +0200

    Create A.cpp

---
+++ Sheet 2 (Conditions)/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c;
+	cin >> c;
+	if (c == 'a' || c == 'o' || c == 'i' || c == 'u' || c == 'e')
+	{
+		cout << "vowel";
+	}
+	else
+	{
+		cout << "consonant";
+	}
+		
+}
