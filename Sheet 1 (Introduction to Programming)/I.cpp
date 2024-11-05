c094a44a63e8c1b3e0dc51da922c4f53f6bcfa7d
cewatlac <128319370+cewatlac@users.noreply.github.com>
2024-11-05 20:52:45 +0200

    Create I.cpp

---
+++ Sheet 1 (Introduction to Programming)/I.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int M, N;
+    cin >> M >> N;
+    int total= M * N;
+    int max= total/ 2;
+    cout << max << endl;
+    return 0;
+}
